3578cb417a2f120bbf31d910277fb03de4f4f598
syseoz <syseoz@naver.com>
2022-09-26 22:52:56 +0900

    add image read

---
+++ demo/src/main/java/com/example/demo/config/CustomSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+
+@Log4j2
+@Configuration
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+}
